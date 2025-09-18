9022505e1480a9946e05940cd55199d982354851
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-09-18 16:45:47 +0200

    JS

---
+++ ejercicios/ejercicios.js
@@ -0,0 +1,23 @@
+// // Le pides al usuario tres números
+// let numero1 = parseInt(prompt("Introduce el primer número: "));
+// let numero2 = parseInt(prompt("Introduce el segundo número: "));
+// let numero3 = parseInt(prompt("Introduce el tercer número: "));
+
+// // Con if-else compruebas si la suma de los números introducidos da 100 o no
+// if (numero1 + numero2 + numero3 == 100){
+//     alert("La suma de los números introduccidos es 100");
+// } else {
+//     alert("La suma de los números introduccidos no es 100");
+// }
+
+// Función setTimeout que pase 3 milisegundos y después muestre un alert de "Hola"
+// setTimeout(() => {alert("Hola");}, 3000);
+
+// Función setTimeout que le pida al usuario su nombre y después de 3 segundos muestre un alert con su nombre
+// let nombreUsuario = prompt("Introduce tu nombre de usuario: ");
+// setTimeout(() => {alert(nombreUsuario);}, 3000);
+
+let cadena = prompt("Introduce una cadena de texto: ");
+console.log(`La cadena tiene ${cadena.length} caracteres`);
+console.log(`La cadena tiene ${cadena.match(/[AEIOUaeiou]/g).length} vocale/s`);
+console.log(`¿La cadena empieza o no por "A"? ${cadena.startsWith("A")}`);
