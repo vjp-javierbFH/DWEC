54abf61a3989c204526e120317633c32512cc65a
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-09-18 17:14:11 +0200

    JS

--- ejercicios/ejercicios.js
+++ ejercicios/ejercicios.js
@@ -20,4 +20,6 @@
 // Le pides al usuario una cadena
 let cadena = prompt("Introduce una cadena de texto: ");
 // Cuenta el número de caracteres
-alert(`La cadena tiene ${cadena.length} caracteres \n La cadena tiene ${cadena.match(/[AEIOUaeiou]/g).length} vocale/s  \n ¿La cadena empieza o no por "A"? ${cadena.startsWith("A")}`);
+alert(`La cadena tiene ${cadena.length} caracteres \n
+    La cadena tiene ${cadena.match(/[AEIOUaeiou]/g).length} vocale/s \n
+    ¿La cadena empieza o no por "A"? ${cadena.startsWith("A")}`);
