a2410507ede05fdd591339a372b48c999124d1d5
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-09-19 16:56:05 +0200

    array

---
+++ ejercicios/js/prueba22.js
@@ -0,0 +1,2 @@
+let a3 = [4, 21, 33, 12, 9, 54];
+console.log(a3.reduce((max, min) => num < max ? num : max, a3[0]));
---
+++ js/funcionesArrays.js
@@ -0,0 +1,22 @@
+// función indexOf() y lastindexOf()
+let array = [3,21,15,61,9,15];
+console.log(array.indexOf(15));
+console.log(array.lastIndexOf(15));
+
+// función every
+console.log(array.every(num => num < 100)); // Devuelve un booleano, en este caso true porque todos los números son menores que 100
+
+// función forEach
+array.forEach(
+
+)
+
+// función map
+let a3 = [4, 21, 33, 12, 9, 54];
+console.log(a3.map(num => num * 2));
+
+// función filter
+console.log(a3.filter(num => num % 2 == 1));
+
+
+
