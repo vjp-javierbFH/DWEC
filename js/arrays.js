ba716854263ccfee24b8ad5c308d9921bb75e263
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-09-19 15:35:04 +0200

    vectores

---
+++ js/arrays.js
@@ -0,0 +1,11 @@
+// Arrays
+// let a = new Array();
+// a[0] = 13;
+// console.log(a.length);
+// console.log(a[0]);
+
+// let a = new Array(12);
+// alert(`Tamaño del vector ${a.length}`);
+// a[20] = "Hola";
+// alert(`Contenido de la posición 20 ${a[20]}
+//     \n Tamaño del vector ${a.length}`);
