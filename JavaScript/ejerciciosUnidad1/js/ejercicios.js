85056d6fbbaa535a9355a647bc781e966f0df332
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-09-25 17:59:02 +0200

    ejercicios

--- JavaScript/ejerciciosUnidad1/js/ejercicios.js
+++ JavaScript/ejerciciosUnidad1/js/ejercicios.js
@@ -87,15 +87,51 @@
 // ----------------------------------------------------------------------------
 
 
-// Ejercicio 4
-console.log("Ejercicio 4: función que reciba 3 parámetros con valores por defecto.");
-console.log("La función convierte las entradas a cadea, entero y entero");
+// // Ejercicio 4
+// console.log("Ejercicio 4: función que reciba 3 parámetros con valores por defecto.");
+// console.log("La función convierte las entradas a cadea, entero y entero");
 
-function funcionConversiones(producto = "Producto genérico", precio = 100, impuesto = 21) {
-    if(NaN(precio))
+// function funcionConversiones(producto = "Producto genérico", precio = 100, impuesto = 21) {
+//     if(NaN(precio))
 
-    console.log(`El producto es ${producto} el precio es ${precio} y su impuesto es ${impuesto}`);
-}
-funcionConversiones();
-funcionConversiones("asd",150,23);
-funcionConversiones();
+//     console.log(`El producto es ${producto} el precio es ${precio} y su impuesto es ${impuesto}`);
+// }
+// funcionConversiones();
+// funcionConversiones("asd",150,23);
+// funcionConversiones();
+
+
+// ----------------------------------------------------------------------------
+
+
+// Ejercicio 5
+
+
+// ----------------------------------------------------------------------------
+
+
+// Ejercicio 6
+
+
+// ----------------------------------------------------------------------------
+
+
+// Ejercicio 7
+
+
+// ----------------------------------------------------------------------------
+
+
+// Ejercicio 8
+
+
+// ----------------------------------------------------------------------------
+
+
+// Ejercicio 9
+
+
+// ----------------------------------------------------------------------------
+
+
+// Ejercicio 10
