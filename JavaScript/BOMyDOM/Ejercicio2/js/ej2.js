b77e0025251efaa15f5188d2d3babbea3a7bf1be
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-10-06 16:43:41 +0200

    ej2

--- JavaScript/BOMyDOM/Ejercicio2/js/ej2.js
+++ JavaScript/BOMyDOM/Ejercicio2/js/ej2.js
@@ -1,9 +1,8 @@
-// Pintar el atributo title del primer enlaces
 let primerEnlace = document.querySelector("#div1 a");
 alert(`Primer enlace que cuelgue de #div1: ${primerEnlace.getAttribute("title")}`);
 
 let enlaceHijo = document.querySelector("div > a");
-alert(`Enlace (a) que sea hijo directo de div: ${enlaceHijo ? enlaceHijo.textContent : "Hijo no encontrado"}`);
+alert(`Enlace (a) que sea hijo directo de div: ${enlaceHijo ? enlaceHijo.textContent : "Hijo no encontrado"}`); // Da error porque los hijos directos de div se considerarían los elementos h1 y p
 
 let enlace = document.querySelector("#div1 > p > a");
-alert(`Enlace (a) hijo de p, también hijo de #div1: ${enlace ? enlace.textContent : "Hijo no encontrado"}`)
+alert(`Enlace (a) hijo de p, también hijo de #div1: ${enlace ? enlace.textContent : "Hijo no encontrado"}`);
