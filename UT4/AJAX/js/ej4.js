84b792e42ba90339955072a53185b8e60b6cf97c
1DAW - Javier Baiyong Fernandez Huidobro <145790505+vjp-javierbFH@users.noreply.github.com>
2025-10-20 14:34:21 +0200

    ajax

--- UT4/AJAX/js/ej4.js
+++ UT4/AJAX/js/ej4.js
@@ -1,14 +1,15 @@
-function pintarInfoRestaurante(objetoRestaurante) {
-    return {
+function pintarInfoRestaurante(objetoRestaurante) { // Función que pinta los datos de los restaurantes
+    return { // Devuelve los datos
         nombre: objetoRestaurante.rdfs_label.value,
-        web: objetoRestaurante.schema_url?.value ? objetoRestaurante.schema_url.value : (objetoRestaurante.uri?.value || "—"),
+        web: objetoRestaurante.schema_url?.value ? objetoRestaurante.schema_url.value : (objetoRestaurante.uri?.value || "—"), // Si no existe una url pintará —
         direccion: objetoRestaurante.schema_address_streetAddress.value || "—",
-        aforo: objetoRestaurante.om_capacidadPersonas?.value ? objetoRestaurante.om_capacidadPersonas.value : ("—")
+        aforo: objetoRestaurante.om_capacidadPersonas?.value ? objetoRestaurante.om_capacidadPersonas.value : ("—") // Lo mismo con url
     };
 }
 
+// Declaro una variable para procesar la pertición
 let peticionAjax = new XMLHttpRequest();
-let restaurantes = [];
+let restaurantes = []; // Un array para procesar los datos en él
 
 peticionAjax.addEventListener("readystatechange", procesarPeticion);
 peticionAjax.open("GET", "https://raw.githubusercontent.com/fredericsangar/backupOpendataCCJSON/master/restaurantes.json");
@@ -21,10 +22,12 @@ function procesarPeticion(event) {
     }
 }
 
+// Función para pintar la información de los restaurantes y guardalos en un array
 function procesarResultado(objetoResultado) {
     restaurantes = objetoResultado.results.bindings.map(pintarInfoRestaurante);
 }
 
+// Función recorre el array restaurantes para meter en el tbody los datos almacenados
 function cargarRestaurantes() {
     const tbody = document.getElementById("tableBody");
     tbody.innerHTML = "";
