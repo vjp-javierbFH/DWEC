fa26492c7c27d526d5a8721c53e56c9b068e7fe8
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-10-23 16:23:13 +0200

    sdfd

--- UT4/FernandezHuidobroJavier_tareas1,2,3/js/ej2.js
+++ UT4/FernandezHuidobroJavier_tareas1,2,3/js/ej2.js
@@ -17,7 +17,6 @@ class Trabajador {
         console.log(`Nombre: ${this.nombre}`);
         console.log(`Horas trabajadas semanalmente: ${this.numHorasSemanales}`);
         console.log(`Salario que gana por hora: ${this.salarioPorHora}`);
-        console.log(`Saldo que gana semanalmente: ${this.getSaldoSemanal()}`);
         console.log("******************");
 
     }
@@ -39,12 +38,13 @@ class Restaurante {
         console.log(`INFORMACIÓN DEL RESTAURANTE: ${this.nombre}`);
 
         for (let i = 0; i < this.trabajadores.length; i++) {
-            console.log(`Trabajador ${i}`);
-            console.log(`El nombre del trabajador es ${this.trabajadores[i].nombre}`);
-            console.log(`El número de horas semanales: ${this.trabajadores[i].numHorasSemanales}`);
-            console.log(`El precio/hora: ${this.trabajadores[i].salarioPorHora}`);
-            
+            this.trabajadores[i].getInfo();
+            this.trabajadores
         }
+
+    }
+    getPagosSemanales() {
+        this.trabajadores.reduce((total, trabajador) => total + trabajador.getSaldoSemanal(), 0);
     }
 }
 
@@ -55,3 +55,4 @@ restaurante.getInfo();
 
 restaurante.anadirTrabajador(new Trabajador("Pepe", 40, 10));
 restaurante.getInfo();
+console.log("Mantener a los trabajadores del restaurante cuesta: " + restaurante.getPagosSemanales());
