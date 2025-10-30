b46ef75d8e2832ec1c6bce142be8feb589611f8c
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-10-30 16:35:13 +0100

    exports

--- UT5-TutorialWebpack1/webpack.config.js
+++ UT5-TutorialWebpack1/webpack.config.js
@@ -1,1 +1,17 @@
-let path = require();
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'mainOut.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html'
+        })
+    ],
+};
