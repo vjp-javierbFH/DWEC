a7f44da1dd88b635d7187c3d791ab5182ba350d4
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-12-12 17:33:35 +0100

    javier

--- UT6/FernandezHuidobroJavier_Ej1-5/Ej1/js/ejercicio1.js
+++ UT6/FernandezHuidobroJavier_Ej1-5/Ej1/js/ejercicio1.js
@@ -1,5 +1,1 @@
-let mapa = L.map("#map").setView([40.037315, -6.085430], 13);
-
-L.tileLayer('http://{$}.title.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyrigtht">OpenStreetMap</a>'
-}).addTo(mapa);
+let mapa = L.map('map').setView([40.037315, -6.085430], 13);
