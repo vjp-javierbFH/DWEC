7a9c90a99baaf9eab855b5feebb069f1caaf3341
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-12-18 17:49:34 +0100

    javier

--- FernandezHuidobrojavier-Proyecto-MiniMarket/src/index.js
+++
@@ -1,7 +0,0 @@
-import { Carrito } from "./js/Carrito.js";
-import { actualizarHeader } from "./js/header";
-
-document.addEventListener("DOMContentLoaded", async () => {
-    // Inicializa el header con contador desde DB
-    await Carrito.actualizarCabeceraCarrito();
-});
